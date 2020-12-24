9f696701e398d95fbed3813688be6b64383b16a3
Makai Post <postmakai@gmail.com>
2020-12-24 11:37:23 -0800

    made a small change to sql seed file

--- employee_tracker.sql
+++ employee_tracker.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
+GRANT ALL PRIVILEGES ON employee_tracker_db.* TO 'vbcode'@'localhost';
 USE employee_tracker_db;
 
 ###########################
@@ -24,8 +25,8 @@ CREATE TABLE employee (
 	id INT AUTO_INCREMENT,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
-	role_id INT,
 	manager_id INT,
+	role_id INT,
 	PRIMARY KEY (id),
 	FOREIGN KEY (role_id) REFERENCES role(id),
 	FOREIGN KEY (manager_id) REFERENCES employee(id)
@@ -50,33 +51,33 @@ INSERT INTO role (title, salary, department_id) VALUES
 	('Legal Team Lead', 250000, 4),
 	('Lawyer', 190000, 4);
     
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-	('Ashley', 'Upshaw', 1, null),
-	('Jeff', 'Schlesinger', 2, 1),
-	('Kera', 'Lopez', 2, 1),
-	('Marian', 'Upshaw', 2, 1),
-	('Sallie', 'Cramer', 2, 1),
-	('Ada', 'Lovelace', 3, null),
-	('Nathan', 'Pardue', 3, null),
-	('Ronald', 'Burch', 4, 6),
-	('Danny', 'Moniz', 4, 6),
-	('Thomas', 'Matthews', 4, 6),
-	('Cynthia', 'Rodriguez', 4, 7),
-	('Pedro', 'Rodriguez', 4, 7),
-	('Grace', 'Braxton', 4, 7),
-	('John', 'Blain', 5, null),
-	('Shannon', 'Wong', 5, null),
-	('Richard', 'Tallman', 6, 14),
-	('Miriam', 'Tillotson', 6, 14),
-	('Jeffery', 'Black', 6, 14),
-	('Cassandra', 'Quinn', 6, 15),
-	('Patrick', 'Andrew', 6, 15),
-	('Evelyn', 'Wu', 7, null),
-	('Rayla', 'Xiong', 7, null),
-	('Minnie', 'Slye', 8, 21),
-	('Delores', 'Crum', 8, 21),
-	('John', 'Cohen', 8, 22),
-	('Jose', 'Vargas', 8, 22);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES
+	('Ashley', 'Upshaw', null, 1),
+	('Jeff', 'Schlesinger', 1, 2),
+	('Kera', 'Lopez', 1, 2),
+	('Marian', 'Upshaw', 1, 2),
+	('Sallie', 'Cramer', 1, 2),
+	('Ada', 'Lovelace', null, 3),
+	('Nathan', 'Pardue', null, 3),
+	('Ronald', 'Burch', 6, 4),
+	('Danny', 'Moniz', 6, 4),
+	('Thomas', 'Matthews', 6, 4),
+	('Cynthia', 'Rodriguez', 7, 4),
+	('Pedro', 'Rodriguez', 7, 4),
+	('Grace', 'Braxton', 7, 4),
+	('John', 'Blain', null, 5),
+	('Shannon', 'Wong', null, 5),
+	('Richard', 'Tallman', 14, 6),
+	('Miriam', 'Tillotson', 14, 6),
+	('Jeffery', 'Black', 14, 6),
+	('Cassandra', 'Quinn', 15, 6),
+	('Patrick', 'Andrew', 15, 6),
+	('Evelyn', 'Wu', null, 7),
+	('Rayla', 'Xiong', null, 7),
+	('Minnie', 'Slye', 21, 8),
+	('Delores', 'Crum', 21, 8),
+	('John', 'Cohen', 22, 8),
+	('Jose', 'Vargas', 22, 8);
 
 ###########################
 ###    CREATE VIEWS     ###
@@ -84,13 +85,29 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 CREATE VIEW employee_view AS
 	SELECT 
 		e.id, 
-		e.first_name, 
-        e.last_name, 
-        r.title, 
-        d.name AS department, 
-        r.salary, 
-        CONCAT(m.first_name, " ", m.last_name) AS manager
+        e.first_name,
+        e.last_name,
+        e.manager_id,
+        CONCAT(m.first_name, " ", m.last_name) AS manager,
+        e.role_id,
+        r.title,
+        r.salary,
+        r.department_id,
+        d.name AS department
 	FROM employee e
 	LEFT JOIN employee m ON e.manager_id = m.id
 	LEFT JOIN role r ON e.role_id = r.id
 	LEFT JOIN department d ON r.department_id = d.id;
+SELECT * FROM employee_view;
+
+CREATE VIEW manager_view AS
+	SELECT *
+	FROM employee_view
+	WHERE id IN (
+		SELECT manager_id
+		FROM employee
+		WHERE manager_id IS NOT NULL
+		GROUP BY manager_id
+	);
+SELECT * FROM manager_view;
+SELECT * FROM employee_display_view WHERE `manager_id` = 6;
